d84f19d0df2fba9041be69c8cc9129788f458221
Eduardo Margulis <edumarg@gmx.com>
2021-06-09 17:24:35 +0300

    added pagination to currencies table

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -6,6 +6,8 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
 import Paper from "@material-ui/core/Paper";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
@@ -29,6 +31,8 @@ const CurrencyTable = () => {
   const deleteCurrency = useContext(DeleteCurrencyContext);
 
   const [openTableMenus, setOpenTableMenus] = useState([]);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [page, setPage] = useState(0);
 
   const handleClickTableMenu = (currency, event) => {
     let tableMenus = [...openTableMenus];
@@ -43,7 +47,6 @@ const CurrencyTable = () => {
     tableMenus[index] = null;
     setOpenTableMenus(tableMenus);
   };
-  console.log("anchors: ", openTableMenus);
 
   const handleTranformBigNum = (num) => {
     if (Math.round(num).toString().length > 12)
@@ -53,6 +56,19 @@ const CurrencyTable = () => {
     if (Math.round(num).toString().length > 6)
       return `${(num / MILLION).toFixed(2)}M`;
     return num.toFixed(2);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    console.log("handle page change");
+    console.log("event", event);
+    console.log(newPage);
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    console.log("handle rowschange");
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
   };
 
   return (
@@ -75,73 +91,88 @@ const CurrencyTable = () => {
           </TableHead>
           <TableBody>
             {currencies &&
-              currencies.map((currency, index) => (
-                <TableRow key={currency.id}>
-                  <TableCell component="th" scope="row">
-                    {currencies.indexOf(currency) + 1}
-                  </TableCell>
-                  <TableCell align="center">
-                    {currency.name}
-                    <span className={classes.grey}>-{currency.symbol}</span>
-                  </TableCell>
-                  <TableCell
-                    align="center"
-                    className={
-                      currency.priceChange1d >= 0
-                        ? classes.positiveChange
-                        : classes.negativeChange
-                    }
-                  >{`${currency.priceChange1d}%`}</TableCell>
-                  <TableCell align="center">
-                    {`$${currency.price.toFixed(2)}`}
-                  </TableCell>
-                  <TableCell align="center">{currency.priceBtc}</TableCell>
-                  <TableCell align="center">
-                    {handleTranformBigNum(currency.marketCap)}
-                  </TableCell>
-                  <TableCell align="center">
-                    {handleTranformBigNum(currency.volume)}
-                  </TableCell>
-                  <TableCell align="center">GRAPH</TableCell>
-                  <TableCell align="center">
-                    <IconButton
-                      aria-label={`${currency.name}-more`}
-                      aria-controls={`${currency.name}-menu`}
-                      aria-haspopup="true"
-                      onClick={(event) => handleClickTableMenu(currency, event)}
-                    >
-                      <MenuIcon />
-                    </IconButton>
-                    <Menu
-                      id={`${currency.name}-menu`}
-                      anchorEl={openTableMenus[index]}
-                      keepMounted
-                      open={Boolean(openTableMenus[index])}
-                      onClose={(event) => handleCloseTableMenu(currency)}
-                    >
-                      <MenuItem
-                        onClick={(event) => {
-                          handleCloseTableMenu(currency);
-                          addToCompare(currency);
-                        }}
+              currencies
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((currency, index) => (
+                  <TableRow key={currency.id}>
+                    <TableCell component="th" scope="row">
+                      {currencies.indexOf(currency) + 1}
+                    </TableCell>
+                    <TableCell align="center">
+                      {currency.name}
+                      <span className={classes.grey}>-{currency.symbol}</span>
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      className={
+                        currency.priceChange1d >= 0
+                          ? classes.positiveChange
+                          : classes.negativeChange
+                      }
+                    >{`${currency.priceChange1d}%`}</TableCell>
+                    <TableCell align="center">
+                      {`$${currency.price.toFixed(2)}`}
+                    </TableCell>
+                    <TableCell align="center">{currency.priceBtc}</TableCell>
+                    <TableCell align="center">
+                      {handleTranformBigNum(currency.marketCap)}
+                    </TableCell>
+                    <TableCell align="center">
+                      {handleTranformBigNum(currency.volume)}
+                    </TableCell>
+                    <TableCell align="center">GRAPH</TableCell>
+                    <TableCell align="center">
+                      <IconButton
+                        aria-label={`${currency.name}-more`}
+                        aria-controls={`${currency.name}-menu`}
+                        aria-haspopup="true"
+                        onClick={(event) =>
+                          handleClickTableMenu(currency, event)
+                        }
                       >
-                        Add to Compare
-                      </MenuItem>
-                      <MenuItem
-                        onClick={() => {
-                          handleCloseTableMenu(currency);
-                          deleteCurrency(currency);
-                        }}
+                        <MenuIcon />
+                      </IconButton>
+                      <Menu
+                        id={`${currency.name}-menu`}
+                        anchorEl={openTableMenus[index]}
+                        keepMounted
+                        open={Boolean(openTableMenus[index])}
+                        onClose={(event) => handleCloseTableMenu(currency)}
                       >
-                        Delete Row
-                      </MenuItem>
-                    </Menu>
-                  </TableCell>
-                </TableRow>
-              ))}
+                        <MenuItem
+                          onClick={(event) => {
+                            console.log(currency);
+                            handleCloseTableMenu(currency);
+                            addToCompare(currency);
+                          }}
+                        >
+                          Add to Compare
+                        </MenuItem>
+                        <MenuItem
+                          onClick={() => {
+                            console.log(currency);
+                            handleCloseTableMenu(currency);
+                            deleteCurrency(currency);
+                          }}
+                        >
+                          Delete Row
+                        </MenuItem>
+                      </Menu>
+                    </TableCell>
+                  </TableRow>
+                ))}
           </TableBody>
         </Table>
       </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25, 50]}
+        component="div"
+        count={currencies.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={(event, newPage) => handleChangePage(event, newPage)}
+        onChangeRowsPerPage={(event) => handleChangeRowsPerPage(event)}
+      />
     </React.Fragment>
   );
 };
