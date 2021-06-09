4d2ac6ae69d23228a5917720f034181ad609da60
Eduardo Margulis <edumarg@gmx.com>
2021-06-09 21:42:34 +0300

    added linear chart to compare currencies

--- src/components/charts.jsx
+++ src/components/charts.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import LineChart from "./linearChart";
 
 const Charts = () => {
   return (
     <React.Fragment>
       <h2>Charts</h2>
+      <LineChart />
     </React.Fragment>
   );
 };
